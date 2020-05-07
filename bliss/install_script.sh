a2068b9f3285782660ebfe19e3c7053c35f97c7a
Linus Pithan <linus.pithan@esrf.fr>
2020-05-07 16:14:55 +0200

    o

--- bliss/install_script.sh
+++ bliss/install_script.sh
@@ -2,4 +2,7 @@
 
 echo -e "\n------------------ bliss install script  ------------------"
 cd /bliss/
+conda config --add channels esrf-bcu
+conda config --append channels conda-forge
+conda config --append channels tango-controls
 conda create --quiet --name demoenv --file requirements-conda.txt --file requirements-test-conda.txt
