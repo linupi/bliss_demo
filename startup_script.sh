c03bb0f3f8b51f417935215d6a6d45edd73dde87
Linus Pithan <linus.pithan@esrf.fr>
2020-05-08 08:24:08 +0200

    use blocking nx

--- startup_script.sh
+++ startup_script.sh
@@ -8,13 +8,6 @@ if [ $status -ne 0 ]; then
   exit $status
 fi
 
-# Start the second process
-/nxserver.sh &
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start my_first_process: $status"
-  exit $status
-fi
+# Start the second process blocking
+/nxserver.sh 
 
-# Go to final shell
-/bin/bash $@
