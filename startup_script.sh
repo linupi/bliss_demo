4a6fc74b0dab7b1b7c350f2d58d30f044c9a32ed
Linus Pithan <linus.pithan@esrf.fr>
2020-05-08 20:03:16 +0200

    try to create display

--- startup_script.sh
+++ startup_script.sh
@@ -2,7 +2,7 @@
 
 echo -e "\n------------------ bliss startup script -v3----------------"
 echo -e "\n------------------ start x1vnc----------------"
-x11vnc -create&
+x11vnc -create -xkb -noxrecord -noxfixes -noxdamage -display :0 -auth /var/run/lightdm/root/:0 -rfbauth /etc/x11vnc.pass -rfbport 5900&
 echo -e "\n------------------ start nx----------------"
 /nxserver.sh &
 echo -e "\n------------------ start nx----------------"
