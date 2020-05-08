84008054bb66b5ba0da0f0111f79469de411d4fa
Linus Pithan <linus.pithan@esrf.fr>
2020-05-08 10:11:45 +0200

    fetch startup script on startup

--- bliss/startup_script.sh
+++ bliss/startup_script.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 
-echo -e "\n------------------ bliss startup script  -2----------------"
+echo -e "\n------------------ bliss startup script  -3----------------"
+cd /bliss_demo
+git fetch
+git reset --hard origin/master
+### all below should be in a separate script i guess so that the 
+### stuff in the git - if updated will be taken into account.
 echo -e "\n------------------ start x1vnc----------------"
 x11vnc &
 echo -e "\n------------------ start nx----------------"
@@ -10,4 +15,6 @@ echo -e "\n------------------ start activate conda----------------"
 cd /bliss/
 source /opt/conda/bin/activate demoenv
 echo -e "\n------------------ start beacon----------------"
-python demo/start_demo_servers.py 
+python demo/start_demo_servers.py &
+echo -e "\n------------------ start shell----------------"
+/bin/bash $@
