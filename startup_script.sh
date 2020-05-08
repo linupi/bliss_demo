1713e5674b3e6125b44110a36b4e0bad1f418814
Linus Pithan <linus.pithan@esrf.fr>
2020-05-08 08:26:30 +0200

    n

--- startup_script.sh
+++ startup_script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Start the first process
-vncserver
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start my_first_process: $status"
-  exit $status
-fi
+vncserver&
+#status=$?
+#if [ $status -ne 0 ]; then
+#  echo "Failed to start my_first_process: $status"
+#  exit $status
+#fi
 
 # Start the second process blocking
 /nxserver.sh 
