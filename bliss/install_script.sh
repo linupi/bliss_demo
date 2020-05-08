313da2d440eadc25c7ae8d63984ac91edbab0c27
Linus Pithan <linus.pithan@esrf.fr>
2020-05-08 18:39:09 +0200

    add silx

--- bliss/install_script.sh
+++ bliss/install_script.sh
@@ -5,6 +5,6 @@ cd /bliss/
 conda config --add channels esrf-bcu
 conda config --append channels conda-forge
 conda config --append channels tango-controls
-conda create --quiet --name demoenv --file requirements-conda.txt --file requirements-test-conda.txt && conda clean -a -y
+conda create --quiet --name demoenv silx --file requirements-conda.txt --file requirements-test-conda.txt && conda clean -a -y
 conda activate demoenv
 pip install --no-deps -e .
