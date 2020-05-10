d2cc2ff418edf171e172a40b6ae957f386a39d1d
Linus Pithan <linus.pithan@esrf.fr>
2020-05-10 18:08:59 +0200

    p

--- bliss/install_script.sh
+++ bliss/install_script.sh
@@ -7,5 +7,6 @@ conda config --add channels esrf-bcu
 conda config --append channels conda-forge
 conda config --append channels tango-controls
 conda create --quiet --name demoenv pymca --file requirements-conda.txt --file requirements-test-conda.txt && conda clean -a -y
-conda activate demoenv
-pip install -e .
+#conda activate demoenv
+source /opt/conda/bin/activate demoenv
+pip install --no-deps --e .
