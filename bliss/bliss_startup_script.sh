a567ffffa41faaf50d32c16d9e29a3ab55b0e7c6
Valentin Valls <valentin.valls@esrf.fr>
2020-06-01 12:35:23 +0200

    Extract mouse pointers

--- bliss/bliss_startup_script.sh
+++ bliss/bliss_startup_script.sh
@@ -3,6 +3,8 @@ HOME=/home/nomachine
 echo -e "\n------------------ copy desktop shortcuts----------------"
 mkdir -p /home/nomachine/Desktop
 cp /bliss_demo/bliss/*.desktop /home/nomachine/Desktop/
+tar -xzf /bliss_demo/assets/160115-bDMZT.tar.gz -C /tmp/
+cp -r /tmp/bDMZT/* /usr/share/icons/
 echo -e "\n------------------ start activate conda----------------"
 cd /bliss/
 source /opt/conda/bin/activate demoenv
